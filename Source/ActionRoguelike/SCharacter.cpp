ae74ab210d5ad3f50b4e319945cfe35ece8ac70c
Yuanyf <yuanyf@dongwu-inc.com>
2022-08-05 15:38:56 +0800

    Added SCharacter & Setup Input
    - Added Character class
    - Added MoveForward/Turn input

--- Source/ActionRoguelike/SCharacter.cpp
+++ Source/ActionRoguelike/SCharacter.cpp
@@ -2,6 +2,8 @@
 
 
 #include "SCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -9,6 +11,11 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
 }
 
 // Called when the game starts or when spawned
@@ -17,6 +24,12 @@ void ASCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 }
+
+void ASCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(),value);
+}
+
 
 // Called every frame
 void ASCharacter::Tick(float DeltaTime)
@@ -29,6 +42,9 @@ void ASCharacter::Tick(float DeltaTime)
 void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward",this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn",this,&APawn::AddControllerYawInput);
 
 }
 
